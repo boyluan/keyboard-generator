cfda844b1e9480d35770f29e347135642074178a
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-02 04:49:30 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -17,11 +17,6 @@ class MyDocument extends Document {
         <body className='bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400'>
           <main className='max-w-3xl mx-auto'>
             <Header />
-            <link
-            as="font"
-            rel="preload"
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@600&display=swap"
-            />
             <Main />
           </main>
           <Footer />
