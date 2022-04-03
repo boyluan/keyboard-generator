229b921639338636087d6f088427cff14a0dfecc
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-03 21:58:59 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -37,6 +37,10 @@ export default function Header() {
                     />
                     </svg>
                     <div className="wallet">
+                        {/* ##
+							// The question mark operator ? takes three operands (contd. below):
+							// 1) some condition 2) a value if that condition is TRUE 3) and a value if that condition is FALSE
+							// It is used in JavaScript to shorten an if else statement to one line of code. */}
                         { connectedAccount != null ? <p> Wallet: {connectedAccount.slice(0, 6)}...{connectedAccount.slice(-4)} </p> : <p> Not connected </p> }
                     </div>
                     </div>
