ee398852c16831586bb01c67566162385038b104
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-03 22:09:49 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,6 @@ import '../styles/globals.css'
 import Header from '../components/header'
 import '../styles/app.css'
 
-import MetaMaskAccountProvider from '../components/meta-mask-account-provider.js'
-
-// import ethLogo from '../assets/ethlogo.png';
-
-// import '../assets/ethlogo.png'
-
 // We'll update this {{ _app.js }} file to include the toast-notification-displayer (contd. below)
 // And this will let us send toasts from anywhere in our dApp, and have them displayed
 import { Toaster } from 'react-hot-toast'
@@ -19,7 +13,7 @@ function MyApp({ Component, pageProps }) {
     // Here, we're wrapping our pages with the provider
     // And this gives them access to this data
     <MetaMaskAccountProvider>
-      <Header/>
+      <Header />
       <Toaster />
       <Component {...pageProps} />
 
