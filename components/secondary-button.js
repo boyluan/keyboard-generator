f2daa203ff450997715ecaafdbddcf6a80b766e1
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-04 01:32:14 +0100

    Update secondary-button.js

--- components/secondary-button.js
+++ components/secondary-button.js
@@ -1,6 +1,6 @@
 export default function SecondaryButton(props) {
   return <button
     {...props}
-    className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-80 disabled:pointer-events-none"
+    className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium font-loader rounded-md text-black bg-blue-100 hover:bg-black hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-300 disabled:opacity-80 disabled:pointer-events-none"
   >{props.children}</button>
 }
