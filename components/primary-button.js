74c8688e4d52fd5fd9de20990a4a01c0d8064cf8
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-04 01:31:47 +0100

    Update primary-button.js

--- components/primary-button.js
+++ components/primary-button.js
@@ -1,7 +1,7 @@
 export default function PrimaryButton(props) {
 
   const { type, children, ...rest } = props
-  const className="max-w-fit inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-80 disabled:pointer-events-none"
+  const className="max-w-fit inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium font-loader rounded-md shadow-sm text-gray-100 bg-black hover:bg-indigo-200 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-300 disabled:opacity-80 disabled:pointer-events-none"
 
   if(type === "link") {
     return <a {...rest} className={className}>{children}</a>
