683ce7623b5d5f2328c360c3aebee77081c8cd71
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-05 13:04:27 +0100

    Add files via upload

---
+++ components/Favicon.tsx
@@ -0,0 +1,31 @@
+// DEFUNCT ## Preserving as template
+
+/* const Favicon = (): JSX.Element => {
+    return (
+        <>
+            <meta charSet="utf-8" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+            <link rel="icon" href="/favicon.ico" /> 
+            <link rel="manifest" href="/site.webmanifest"/>
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+
+            <meta
+                name="description"
+                content="Web site created using create-next-app"
+            />
+
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#ffffff"/>
+
+            <title>Keyboard Generator</title>
+            <meta name="title" content="An on-chain keyboard generator" />
+            <meta name="description" content="Generate a Keyboard on Ethereum's testnet" />
+        </>
+    );
+}
+
+export default Favicon;  */
