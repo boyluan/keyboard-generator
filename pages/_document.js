d8ebd92699d07e60b1a9dbff98b61ac04d153d88
solaxds <67832977+solaxds@users.noreply.github.com>
2022-04-05 13:03:05 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 import Footer from '../components/footer'
 
-// import Header from '../components/header'
+// import Favicon from '../components/Favicon.tsx'; {{ DEFUNCT }}
 
 import MetaMaskAccountProvider from '../components/meta-mask-account-provider'
 
@@ -16,25 +16,24 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <MetaMaskAccountProvider>
-        <Head />
-        <body className='bg-gradient-to-r from-gray-400 via-red-100 to-blue-100'>
-          <main className='max-w-3xl mx-auto'>
-            <link
-            as="font"
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@600&display=swap"
-            />
-            <Main />
-          </main>
-          <Footer />
-          <NextScript />
-
-        </body>
+           <Head />
+                <body className='bg-gradient-to-r from-gray-400 via-red-100 to-blue-100'>
+                  <main className='max-w-3xl mx-auto'>
+                    <link
+                    as="font"
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@600&display=swap"
+                    />
+                  <Main />
+                </main>
+              <Footer />
+            <NextScript />
+          </body>
         </MetaMaskAccountProvider>
       </Html>
-    )
+    );
   }
 }
 
